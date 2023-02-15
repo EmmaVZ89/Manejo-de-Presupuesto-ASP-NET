c5aade576c6ef997552cb3875ac85570959a30c2
Emmanuel Zelarayan <emmavz@live.com>
2023-02-15 20:56:18 -0300

    add forms controller

---
+++ Manejo Presupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,46 @@
+﻿using Manejo_Presupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Manejo_Presupuesto.Servicios;
+
+namespace Manejo_Presupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            tipoCuenta.UsuarioId = 1;
+
+            var yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
+
+            if (yaExisteTipoCuenta)
+            {
+                ModelState.AddModelError(nameof(tipoCuenta.Nombre),
+                    $"El nombre {tipoCuenta.Nombre} ya existe.");
+
+                return View(tipoCuenta);
+            }
+
+            await this.repositorioTiposCuentas.Crear(tipoCuenta);
+
+            return View();
+        }
+    }
+}
