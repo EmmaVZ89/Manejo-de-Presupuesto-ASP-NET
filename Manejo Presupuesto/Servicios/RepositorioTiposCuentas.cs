eb081c612044950ea5ef0967de1b732811b72d2c
Emmanuel Zelarayan <emmavz@live.com>
2023-02-17 20:55:24 -0300

    add update and delete

--- Manejo Presupuesto/Servicios/RepositorioTiposCuentas.cs
+++ Manejo Presupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -6,9 +6,14 @@ namespace Manejo_Presupuesto.Servicios
 {
     public interface IRepositorioTiposCuentas
     {
+        Task Actualizar(TipoCuenta tipoCuenta);
+        Task Borrar(int id);
         Task Crear(TipoCuenta tipoCuenta);
 
         Task<bool> Existe(string nombre, int usuarioId);
+        Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId);
+        Task<TipoCuenta> ObtenerPorId(int id, int usuarioId);
+        Task Ordenar(IEnumerable<TipoCuenta> tipoCuentasOrdenados);
     }
 
     public class RepositorioTiposCuentas : IRepositorioTiposCuentas
@@ -34,8 +39,49 @@ namespace Manejo_Presupuesto.Servicios
             using var connection = new SqlConnection(connectionString);
             var existe = await connection.QueryFirstOrDefaultAsync<int>(@"SELECT 1 FROM TiposCuentas
                                                                         WHERE Nombre = @Nombre AND UsuarioId = @UsuarioId",
-                                                                        new {nombre, usuarioId});
+                                                                        new { nombre, usuarioId });
             return existe == 1;
+        }
+
+        public async Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            {
+                return await connection.QueryAsync<TipoCuenta>(@"SELECT Id, Nombre, Orden
+                                                                FROM TiposCuentas
+                                                                WHERE UsuarioId = @UsuarioId
+                                                                ORDER BY Orden", new { usuarioId });
+            }
+        }
+
+        public async Task Actualizar(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"UPDATE TiposCuentas
+                                            SET Nombre = @Nombre
+                                            WHERE Id = @Id", tipoCuenta);
+        }
+
+        public async Task<TipoCuenta> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<TipoCuenta>(@"SELECT Id, Nombre, Orden
+                                                                       FROM TiposCuentas
+                                                                       WHERE Id = @Id AND UsuarioId = @UsuarioId",
+                                                                        new { id, usuarioId });
+        }
+
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"DELETE TiposCuentas WHERE Id = @Id", new { id });
+        }
+
+        public async Task Ordenar(IEnumerable<TipoCuenta> tipoCuentasOrdenados)
+        {
+            var query = "UPDATE TiposCuentas SET Orden = @Orden WHERE Id = @Id";
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(query, tipoCuentasOrdenados);
         }
     }
 }
