b1e5d4ddd236e69a7d1a63c764ee6817fc73300b
Emmanuel Zelarayan <emmavz@live.com>
2023-02-19 01:29:50 -0300

    add transacciones CRUD

--- Manejo Presupuesto/Program.cs
+++ Manejo Presupuesto/Program.cs
@@ -7,8 +7,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
 builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
 builder.Services.AddTransient<IRepositorioCuentas, RepositorioCuentas>();
-builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddTransient<IRepositorioCategorias, RepositorioCategorias>();
+builder.Services.AddTransient<IRepositorioTransacciones, RepositorioTransacciones>();
+builder.Services.AddAutoMapper(typeof(Program));
+
 
 var app = builder.Build();
 
--- Manejo Presupuesto/Servicios/AutoMapperProfiles.cs
+++ Manejo Presupuesto/Servicios/AutoMapperProfiles.cs
@@ -8,6 +8,7 @@ namespace Manejo_Presupuesto.Servicios
         public AutoMapperProfiles()
         {
             CreateMap<Cuenta, CuentaCreacionViewModel>();
+            CreateMap<TransaccionActualizacionViewModel, Transaccion>().ReverseMap();
         }
     }
 }
