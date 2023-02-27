e7b1909ce7d46afa9428d1ad9a3ec50cd2fddc47
Emmanuel Zelarayan <emmavz@live.com>
2023-02-27 17:59:05 -0300

    change styles in login and register

--- Manejo Presupuesto/Models/LoginViewModel.cs
+++ Manejo Presupuesto/Models/LoginViewModel.cs
@@ -4,10 +4,10 @@ namespace Manejo_Presupuesto.Models
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Required(ErrorMessage = "El campo es requerido")]
         [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Required(ErrorMessage = "El campo es requerido")]
         public string Password { get; set; }
         public bool Recuerdame { get; set; }
     }
